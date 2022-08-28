61df76813ecbbd572ed8f540130515ee5283b7bc
callmeglenn <glenn.marcell.tjandra@gmail.com>
2022-08-28 14:47:25 +0800

    Compiled typescript files

--- lib/classes/Collection.d.ts
+++ lib/classes/Collection.d.ts
@@ -5,5 +5,7 @@ declare class Collection {
     constructor(...series: Array<Series>);
     create(series: string, card: string): DataRead;
     get(id: string): DataRead | null;
+    /** Retrieve newly created card(s) by random */
+    random(number: number): DataRead | Array<DataRead>;
 }
 export default Collection;
